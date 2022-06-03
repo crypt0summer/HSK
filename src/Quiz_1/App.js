eb89d50b639029342ba695f344bd85ff4ee21439
April <realapril25@gmail.com>
2022-06-04 05:54:33 +0900

    Add local json file reading

--- src/Quiz_1/App.js
+++ src/Quiz_1/App.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { View, Text, StyleSheet, SafeAreaView, Button } from "react-native";
+import customData from '../../assets/json/data_kor.json';
 
 function Quiz_1_Screen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>q1 Screen</Text>
+      <Text>{JSON.stringify(customData.hsk_2022)}</Text>
     </View>
   );
 }
